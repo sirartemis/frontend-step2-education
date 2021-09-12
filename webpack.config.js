53b429c1708b3482b7572bc83032f7c34b576ae4
Artem Snytko <sirartemis91@gmail.com>
2021-09-13 01:36:53 +0300

    Поигрался с кэшированием и прочими прелестями, ничего не понял, но вроде работает, разбивает на куски конечный продукт и кэширует - в итоге страничка будет прогружаться еще быстрее

--- webpack.config.js
+++ webpack.config.js
@@ -3,23 +3,33 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
-	entry: {
-		index: './src/index.js',
-		print: './src/print.js',
-	},
+	entry: './src/index.js',
 	devtool: 'inline-source-map',
 	devServer: {
 		static: './dist',
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: 'Development',
+			title: 'Caching',
 		}),
 	],
 	output: {
-		filename: '[name].bundle.js',
+		filename: '[name].[contenthash].js',
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
 		publicPath: '/',
+	},
+	optimization: {
+		moduleIds: 'deterministic',
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+			},
+		},
 	},
 };
