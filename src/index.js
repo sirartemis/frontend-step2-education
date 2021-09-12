3b7cf1cefbd324d90037521b892d0dd5997a375d
Artem Snytko <sirartemis91@gmail.com>
2021-09-12 20:28:31 +0300

    Настроил работу с изображениями, проверил - всё работает

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Calendar from './calendar.png';
 
 function component() {
 	const element = document.createElement('div');
@@ -8,6 +9,12 @@ function component() {
 	
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 	element.classList.add('hello');
+
+	// Add the image to our existing div.
+	const myCalendar = new Image();
+	myCalendar.src = Calendar;
+
+	element.appendChild(myCalendar);
 
 	return element;
 }
