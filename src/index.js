544dd24cc7f12c803f1d1f640a8d7d2a05e616af
Artem Snytko <sirartemis91@gmail.com>
2021-09-12 22:11:59 +0300

    удалил все лишнее, что узнать больше о webpack далее по гайду output management

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Calendar from './calendar.png';
 
 function component() {
 	const element = document.createElement('div');
 
-	// Loadash, now imported by this script
 	
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
-
-	// Add the image to our existing div.
-	const myCalendar = new Image();
-	myCalendar.src = Calendar;
-
-	element.appendChild(myCalendar);
 
 	return element;
 }
