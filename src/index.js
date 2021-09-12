f2768cd46174274860662d601e90be07c23cba3a
Artem Snytko <sirartemis91@gmail.com>
2021-09-12 20:14:10 +0300

    Добавил обработчики стилей - style-loader и css-loader, проверил, все работает

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import './style.css';
 
 function component() {
 	const element = document.createElement('div');
@@ -6,6 +7,7 @@ function component() {
 	// Loadash, now imported by this script
 	
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+	element.classList.add('hello');
 
 	return element;
 }
