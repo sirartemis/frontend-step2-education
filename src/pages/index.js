f9020af5af14893e964604edd366dc6390da8157
Artem Snytko <sirartemis91@gmail.com>
2021-10-01 02:07:33 +0300

    Доработка логики кнопки "очистить"

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,25 @@ $('.dropdown').find('input').on("click", e => checkGuests(e));
 $('.dropdown').find('input').trigger("input");
 $('.dropdown').find('input').trigger("click");
 
+// Логика кнопки "очистить"
+
+$('.clear').click(function (e) {
+
+  let target = $(e.target).parents('.dropdown').find('.input-sizer')
+
+  target.find('input').val('0');
+  target.attr('data-value','0');
+
+  removeClearHandler(e);
+
+});
+
+// Логика проверки ввода пользователя
+// и отрисовки кнопки "очистить"
 
   function checkGuests(e){
 
   // Проверяем число в месте активности пользователя
-
-  let number = $(e.target).parents('.dropdown').find('.input-sizer').find('input').val();
-
-
-  // Если хоть где-то добавился гость
-
-  if ( number > 0 ) {
-    addClearHandler(e); // Добавляем кнопку "очистить"
-  };
 
   // Взрослые
 
@@ -41,10 +47,17 @@ $('.dropdown').find('input').trigger("click");
 
   let res = parseInt(adults) + parseInt(children) + parseInt(babies);
 
+
+  // Если хоть где-то добавился гость
+
+  if ( res > 0 ) {
+    addClearHandler(e); // Добавляем кнопку "очистить"
+  };
+
   // Если гостей нет, убираем кнопку "очистить"
 
   if ( res  === 0 ){
+
     removeClearHandler(e);
   };
 };
-
