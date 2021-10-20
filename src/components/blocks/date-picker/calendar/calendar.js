92c59257b4af6fa792ccc4cb6018cc810b78ba09
Artemis <sirartemis91@gmail.com>
2021-10-20 21:28:19 +0300

    date-picker-step2

--- src/components/blocks/date-picker/calendar/calendar.js
+++ src/components/blocks/date-picker/calendar/calendar.js
@@ -1,4 +1,5 @@
 import AirDatepicker from "air-datepicker";
+import { addClearHandler, removeClearHandler } from "../../dropdown/__btns/__btns";
 
 const calendars = document.querySelectorAll('.calendar');
 
@@ -11,7 +12,36 @@ calendars.forEach(calendar => {
     nextHtml: "<span class='material-icons'>arrow_forward</span>",
     range:true,
   });
+  const execute = document.querySelector('.execute');
+  execute.addEventListener('click', e => {
+    const dropDownButtons = e.target.parentNode;
+    const dropDownMenu = dropDownButtons.parentNode;
+    const dropDown = dropDownMenu.parentNode;
+    const firstSelect = dropDown.querySelector('.first-select');
+    const secondSelect = dropDown.querySelector('.second-select');
+
+    const firstDate = cal.selectedDates[0];
+    const secondDate = cal.selectedDates[1];
+
+    const dateToString = date => {
+      return cal.formatDate(date,'dd.MM.yyyy')
+    }
+
+    const firstDateString = dateToString(firstDate);
+    const secondDateString = dateToString(secondDate);
+
+    firstSelect.innerHTML = firstDateString;
+    secondSelect.innerHTML = secondDateString;
+
+    console.log(cal.selectedDates.length);
+
+  });
+
   calendar.addEventListener('click', e => {
-    cal.setViewDate(e);
+    if (cal.selectedDates.length > 0) {
+      addClearHandler(e);
+      console.log(calendar)
+    }
   })
+
 });
